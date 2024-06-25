d5f93f474ff772ce3963f35adca1eab22b7072f2
Neal Terrell <nealdt@gmail.com>
2024-06-25 10:09:19 -0700

    Starter code for homework 6.

---
+++ README.md
@@ -0,0 +1,3 @@
+# animator
+
+A starter program for constructing Animation objects to be applied by an Animator.
---
+++ src/main.cpp
@@ -0,0 +1,153 @@
+#define _USE_MATH_DEFINES
+#include <glad/glad.h>
+#include <iostream>
+#include <memory>
+#include <filesystem>
+#include <math.h>
+
+#include "AssimpImport.h"
+#include "Mesh3D.h"
+#include "Object3D.h"
+#include "Animator.h"
+#include "ShaderProgram.h"
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/Window.hpp>
+
+struct Scene {
+	ShaderProgram program;
+	std::vector<Object3D> objects;
+	std::vector<Animator> animators;
+};
+
+/**
+ * @brief Constructs a shader program that performs texture mapping with no lighting.
+ */
+ShaderProgram texturingShader() {
+	ShaderProgram shader;
+	try {
+		shader.load("shaders/texture_perspective.vert", "shaders/texturing.frag");
+	}
+	catch (std::runtime_error& e) {
+		std::cout << "ERROR: " << e.what() << std::endl;
+		exit(1);
+	}
+	return shader;
+}
+
+/**
+ * @brief Loads an image from the given path into an OpenGL texture.
+ */
+Texture loadTexture(const std::filesystem::path& path, const std::string& samplerName = "baseTexture") {
+	StbImage i;
+	i.loadFromFile(path.string());
+	return Texture::loadImage(i, samplerName);
+}
+
+/*****************************************************************************************
+*  DEMONSTRATION SCENES
+*****************************************************************************************/
+Scene bunny() {
+	Scene scene{ texturingShader() };
+
+	// We assume that (0,0) in texture space is the upper left corner, but some artists use (0,0) in the lower
+	// left corner. In that case, we have to flip the V-coordinate of each UV texture location. The last parameter
+	// to assimpLoad controls this. If you load a model and it looks very strange, try changing the last parameter.
+	auto bunny = assimpLoad("models/bunny_textured.obj", true);
+	bunny.grow(glm::vec3(9, 9, 9));
+	bunny.move(glm::vec3(0.2, -1, 0));
+	
+	// Move all objects into the scene's objects list.
+	// Because the bunny was constructed locally, it will be destroyed when we leave this scope.
+	// "Moving" an object in C++ is complicated, but here, it puts the bunny in the scene's object list
+	// so we can reference it outside of this scope. (Like when we tick() animations in the main.)
+	scene.objects.push_back(std::move(bunny));
+	// Now the "bunny" variable is empty; if we want to refer to the bunny object, we need to reference 
+	// scene.objects[0]
+
+	Animator spinBunny;
+	// Spin the bunny 360 degrees over 10 seconds.
+	spinBunny.addAnimation(std::make_unique<RotationAnimation>(scene.objects[0], 10.0, glm::vec3(0, 2 * M_PI, 0)));
+	
+
+	// Move all animators into the scene's animators list.
+	scene.animators.push_back(std::move(spinBunny));
+
+	return scene;
+}
+
+int main() {
+	
+	std::cout << std::filesystem::current_path() << std::endl;
+	
+	// Initialize the window and OpenGL.
+	sf::ContextSettings settings;
+	settings.depthBits = 24; // Request a 24 bits depth buffer
+	settings.stencilBits = 8;  // Request a 8 bits stencil buffer
+	settings.antialiasingLevel = 2;  // Request 2 levels of antialiasing
+	settings.majorVersion = 3;
+	settings.minorVersion = 3;
+	sf::Window window(sf::VideoMode{ 1200, 800 }, "Modern OpenGL", sf::Style::Resize | sf::Style::Close, settings);
+
+	gladLoadGL();
+	glEnable(GL_DEPTH_TEST);
+
+	// Inintialize scene objects.
+	auto myScene = bunny();
+	// You can directly access specific objects in the scene using references.
+	auto& firstObject = myScene.objects[0];
+
+	// Activate the shader program.
+	myScene.program.activate();
+
+	// Set up the view and projection matrices.
+	glm::vec3 cameraPos = glm::vec3(0, 0, 5);
+	glm::mat4 camera = glm::lookAt(cameraPos, glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+	glm::mat4 perspective = glm::perspective(glm::radians(45.0), static_cast<double>(window.getSize().x) / window.getSize().y, 0.1, 100.0);
+	myScene.program.setUniform("view", camera);
+	myScene.program.setUniform("projection", perspective);
+	myScene.program.setUniform("cameraPos", cameraPos);
+
+	// Ready, set, go!
+	bool running = true;
+	sf::Clock c;
+	auto last = c.getElapsedTime();
+
+	// Start the animators.
+	for (auto& anim : myScene.animators) {
+		anim.start();
+	}
+
+	while (running) {
+		
+		sf::Event ev;
+		while (window.pollEvent(ev)) {
+			if (ev.type == sf::Event::Closed) {
+				running = false;
+			}
+		}
+		auto now = c.getElapsedTime();
+		auto diff = now - last;
+		std::cout << 1 / diff.asSeconds() << " FPS " << std::endl;
+		last = now;
+
+		// Update the scene.
+		for (auto& anim : myScene.animators) {
+			anim.tick(diff.asSeconds());
+		}
+
+		// Clear the OpenGL "context".
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		// Render the scene objects.
+		for (auto& o : myScene.objects) {
+			o.render(myScene.program);
+		}
+		window.display();
+
+
+		
+	}
+
+	return 0;
+}
+
+
